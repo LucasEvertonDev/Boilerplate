92a4295ebf4839f15f8727dd095b2a4958d3ae4e
Lcs Everton <lcseverton@outlook.com>
2023-08-06 11:10:48 -0300

    Formatando query

--- Boilerplate.Infra/Boilerplate.Infra.Data/Contexts/Repositorys/ClientRepository.cs
+++ Boilerplate.Infra/Boilerplate.Infra.Data/Contexts/Repositorys/ClientRepository.cs
@@ -18,29 +18,34 @@ public class ClientRepository : Repository<Client>, ICreateClientRepository, IUp
     public async Task<IEnumerable<Client>> FindAll(SearchClientsModel searchClientsModel)
     {
         return await base.Get().AsNoTracking()
-                .Where(a => (string.IsNullOrEmpty(searchClientsModel.Cpf) || searchClientsModel.Cpf == a.Cpf) &&
-                    (string.IsNullOrEmpty(searchClientsModel.Name) || a.Name.Contains(searchClientsModel.Name)) &&
-                    (string.IsNullOrEmpty(searchClientsModel.Id) || searchClientsModel.Id == a.Id.ToString())
-                ).ToListAsync();
+            .Where(a => 
+                (string.IsNullOrEmpty(searchClientsModel.Cpf) || searchClientsModel.Cpf == a.Cpf) &&
+                (string.IsNullOrEmpty(searchClientsModel.Name) || a.Name.Contains(searchClientsModel.Name)) &&
+                (string.IsNullOrEmpty(searchClientsModel.Id) || searchClientsModel.Id == a.Id.ToString())
+            )
+            .ToListAsync();
     }
 
 
     public async Task<PagedResult<Client>> FindAll(PaginationOptions<SearchClientsModel> options)
     {
-        var totalElements = await _applicationDbContext.Clients.AsNoTracking()
-            .Where(a => (string.IsNullOrEmpty(options.Parameter.Cpf) || options.Parameter.Cpf == a.Cpf) &&
-                    (string.IsNullOrEmpty(options.Parameter.Name) || a.Name.Contains(options.Parameter.Name)) &&
-                    (string.IsNullOrEmpty(options.Parameter.Id) || options.Parameter.Id == a.Id.ToString())
-            ).CountAsync();
+        var totalElements = await base.Get()
+            .Where(a => 
+                (string.IsNullOrEmpty(options.Parameter.Cpf) || options.Parameter.Cpf == a.Cpf) &&
+                (string.IsNullOrEmpty(options.Parameter.Name) || a.Name.Contains(options.Parameter.Name)) &&
+                (string.IsNullOrEmpty(options.Parameter.Id) || options.Parameter.Id == a.Id.ToString())
+            )
+            .CountAsync();
 
-        var itens = await base.Get().AsNoTracking()
-                .Where(a => (string.IsNullOrEmpty(options.Parameter.Cpf) || options.Parameter.Cpf == a.Cpf) &&
-                    (string.IsNullOrEmpty(options.Parameter.Name) || a.Name.Contains(options.Parameter.Name)) &&
-                    (string.IsNullOrEmpty(options.Parameter.Id) || options.Parameter.Id == a.Id.ToString())
-                )
-                .Skip((options.PageNumber - 1) * options.PageSize)
-                .Take(options.PageSize)
-                .ToListAsync();
+        var itens = await base.Get()
+            .Where(a =>
+                (string.IsNullOrEmpty(options.Parameter.Cpf) || options.Parameter.Cpf == a.Cpf) &&
+                (string.IsNullOrEmpty(options.Parameter.Name) || a.Name.Contains(options.Parameter.Name)) &&
+                (string.IsNullOrEmpty(options.Parameter.Id) || options.Parameter.Id == a.Id.ToString())
+            )
+            .Skip((options.PageNumber - 1) * options.PageSize)
+            .Take(options.PageSize)
+            .ToListAsync();
 
         return new PagedResult<Client>(itens, options.PageNumber, options.PageSize, totalElements);
     }
